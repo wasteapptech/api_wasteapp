89b7d77c77b2221db245ad8a175bdb2e00de4b9a
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-02-19 22:36:26 +0700

    Feat: Update cors

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const cors = require('cors');
 const app = express();
 connectDB();
 app.use(express.json());
-app.use(cors({ origin: '*' }));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
 app.get("/", (req, res) => {
     res.send({
         message: "Api Wasteapp",
