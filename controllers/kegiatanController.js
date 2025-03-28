323f58eb8a5dfcfa1549ba73a8999d06c5c4b9f9
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-03-28 13:43:24 +0700

    Feat: Fix typo bgst

--- controllers/kegiatanController.js
+++ controllers/kegiatanController.js
@@ -12,60 +12,10 @@ exports.tambahKegiatan = async (req, res) => {
     }
 };
 
-// Ambil Semua Kegiatan 
+// Ambil Semua Kegiatan (sorted by _id descending)
 exports.getKegiatan = async (req, res) => {
     try {
         const kegiatanList = await Kegiatan.find().sort({ _id: -1 }); 
-        res.json(kegiatanList);
-    } catch (err) {
-        res.status(500).json({ message: "Gagal mengambil kegiatan" });
-    }
-};
-
-
-//  Update Kegiatan
-exports.updateKegiatan = async (req, res) => {
-    const { id } = req.params;
-    const { nama, tanggal, deskripsi } = req.body;
-
-    try {
-        const kegiatanUpdate = await Kegiatan.findByIdAndUpdate(
-            id,
-            { nama, tanggal, deskripsi },
-            { new: true }
-        );
-
-        if (!kegiatanUpdate) {
-            return res.status(404).json({ message: "Kegiatan tidak ditemukan" });
-        }
-
-        res.json({ message: "Kegiatan berhasil diperbarui", kegiatan: kegiatanUpdate });
-    } catch (err) {
-        res.status(500).json({ message: "Gagal memperbarui kegiatan" });
-    }
-};
-
-// Hapus Kegiatan
-exports.hapusKegiatan = async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const kegiatanHapus = await Kegiatan.findByIdAndDelete(id);
-
-        if (!kegiatanHapus) {
-            return res.status(404).json({ message: "Kegiatan tidak ditemukan" });
-        }
-
-        res.json({ message: "Kegiatan berhasil dihapus" });
-    } catch (err) {
-        res.status(500).json({ message: "Gagal menghapus kegiatan" });
-    }
-};
-
-//  Ambil Semua Kegiatan
-exports.getKegiatan = async (req, res) => {
-    try {
-        const kegiatanList = await Kegiatan.find();
         res.json(kegiatanList);
     } catch (err) {
         res.status(500).json({ message: "Gagal mengambil kegiatan" });
@@ -81,7 +31,7 @@ exports.updateKegiatan = async (req, res) => {
         const kegiatanUpdate = await Kegiatan.findByIdAndUpdate(
             id,
             { nama, tanggal, deskripsi },
-            { new: true } // Mengembalikan data yang sudah diperbarui
+            { new: true }
         );
 
         if (!kegiatanUpdate) {
