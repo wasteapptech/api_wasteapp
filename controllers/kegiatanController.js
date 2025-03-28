e8cc7397af50b5a351fd8f75b1b75a301e16de41
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-03-28 13:36:08 +0700

    Feat: Fix typo

--- controllers/kegiatanController.js
+++ controllers/kegiatanController.js
@@ -1,18 +1,5 @@
 const Kegiatan = require("../models/kegiatan");
 
-//  Tambah Kegiatan
-exports.tambahKegiatan = async (req, res) => {
-    const { nama, tanggal, deskripsi } = req.body;
-    try {
-        const kegiatanBaru = new Kegiatan({ nama, tanggal, deskripsi });
-        await kegiatanBaru.save();
-        res.status(201).json({ message: "Kegiatan berhasil ditambahkan", kegiatan: kegiatanBaru });
-    } catch (err) {
-        res.status(500).json({ message: "Gagal menambahkan kegiatan" });
-    }
-};
-const Kegiatan = require("../models/kegiatan");
-
 // Tambah Kegiatan
 exports.tambahKegiatan = async (req, res) => {
     const { nama, tanggal, deskripsi } = req.body;
