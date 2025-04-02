8eb133b3e41f550d1bcc065372cde5aa64efbd5e
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-04-02 14:07:15 +0700

    Feat: Change token

--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,16 @@
 const admin = require('firebase-admin');
-const path = require('path');
 
+// Properly handle newlines in private key
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Important for line breaks
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-  });
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: privateKey
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
 
 const db = admin.database();
-
-module.exports = {
-  admin,
-  db
-};
+module.exports = { admin, db };
