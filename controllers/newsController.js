a0b41552372535de6354585d8f793aa245fa3baa
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-04-04 16:18:57 +0700

    Feat: Fix minor bug

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -18,22 +18,6 @@ exports.getAllNews = async (req, res) => {
 exports.fetchNewsFromAPI = async (req, res) => {
     try {
         const result = await newsService.fetchNewsAndUpdateDatabase();
-<<<<<<< HEAD
-=======
-        
-        if (result.newItems && result.newItems.length > 0) {
-            result.newItems.sort((a, b) => {
-                return new Date(b.scrapedAt) - new Date(a.scrapedAt);
-            });
-            
-            await notificationService.sendNotificationToAllDevices(
-                `${result.newItems.length} Berita Baru`,
-                'Ada berita terbaru yang tersedia!',
-                result.newItems[0]?.imageUrl
-            );
-        }
-        
->>>>>>> 20c97e700700f9028c76b77ae6fd3740db3c9107
         res.status(200).json(result);
     } catch (error) {
         console.error('Error fetching news from API:', error);
